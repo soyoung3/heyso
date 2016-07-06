1d368f97178f4c12d7a273535a138983e1f8ce6e
soyoung3 <makemeha3@gmail.com>
2016-07-07 06:38:58 +0900

    join 추가

---
+++ Chapter06/joinin/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('form', function(req, res, next) {
+  res.render('join-Form', { title: 'Express' });
+});
+
+module.exports = router;
